8508f782ba566020a370cad70d382afcd5602fb1
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-17 17:13:21 +0700

    add class mahasiswa

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -2,6 +2,22 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+    static int nim;
+    int id;
+    string nama;
+
+public:
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; } /*Definisi Function*/
+    static int getNim() { return nim; }         /*Definisi Function*/
+
+    mahasiswa(string pNama) : nama(pNama) { setID(); }
+};
+
 int main(){
-    
+
 }
