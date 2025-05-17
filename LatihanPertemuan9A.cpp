097244d476952b1c306bacd4e2d91c3841820ff0
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-17 18:30:30 +0700

    add friend class, Constructor Mahasiswa

--- LatihanPertemuan9A.cpp
+++ LatihanPertemuan9A.cpp
@@ -2,12 +2,19 @@
 #include <string>
 using namespace std;
 
-class Mhasasiswa{
+class Mahasiswa{
 private:
     string nama;
-    string NIM;
+    string nim;
     float nilai;
-    
+public:
+    Mahasiswa(string n, string i) : nama(n), nim(i), nilai(0) {}
+    void tampil() {
+        cout << "Nama Mahasiswa : " << nama << endl;
+        cout << "NIM : " << nim << endl;
+        cout << "Nilai : " << nilai << endl << endl;
+    }
+    friend class Dosen;
 };
 
 int main(){
