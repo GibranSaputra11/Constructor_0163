eeaa19a206b4973c8ed43c02b3cea8e3f78cbbf8
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-17 17:02:31 +0700

    add file konstruktor

--- destruktor.cpp
+++ destruktor.cpp
@@ -45,4 +45,4 @@ int main(){
     angka belajarcpp(3); // Constructor Dipanggil
     angka *ptrBelajarcpp = new angka(5); // Constructor Dipanggil
     delete ptrBelajarcpp; // Destructor Dipanggil
-}
+} // Destructor Dipanggil
