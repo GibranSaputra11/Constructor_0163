b7125405c91dd22cf9bf810606c919c96e35bdd1
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-17 17:09:41 +0700

    add prosedur untuk menampilkan nama dan nim

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -30,6 +30,12 @@ mahasiswa::mahasiswa(string iNama) {
 mahasiswa::mahasiswa(int iNim, string iNama) {
     nim = iNim;
     nama = iNama;
+}
+
+void mahasiswa::cetak() {
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
 
 int main() {
 
