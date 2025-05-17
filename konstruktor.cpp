10c5b8a252125baace97af16894307a8d9c96082
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-17 17:06:18 +0700

    add class mahasiswa

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -2,7 +2,19 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+    int nim;
+    string nama;
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
 
 
 int main() {
+
 }
